7580b86bc49eef046aac2f5f597f0ac1f70a1bf4
SATYAKI  KC <satyakikc2@gmail.com>
2020-12-28 12:18:35 +0530

    Delete shopping_bill.py

--- shopping_bill.py
+++
@@ -1,8 +0,0 @@
-sum = 0
-while(True):
-    user=input("Enter the item price or press q to quit:")
-    if(user != "q"):
-        sum=sum+int(user)
-        print(f"Your total order cost till now is {sum}")
-    else:
-        print(f"Your total bill is {sum}. \n Thank you for shopping with us")
